c366da1c4eda1d46c1f5501a2fc2c7dc94a5131b
Sungwook Yeom <krazyeom@gmail.com>
2026-05-05 10:23:21 +0900

    style: 영문 상태 태그 가운데 정렬 적용

--- cashnote_readable.js
+++ cashnote_readable.js
@@ -172,9 +172,9 @@
                 detailedOutput += `[${k}]\n`;
                 items.forEach(i => {
                     let icon = '✅';
-                    let tag = '[AVAILABLE]';
-                    if (i.status === 'USED') { icon = '➖'; tag = '[USED     ]'; }
-                    if (i.status === 'CANCELED') { icon = '❌'; tag = '[CANCELED ]'; }
+                    let tag = '[ AVAILABLE ]';
+                    if (i.status === 'USED') { icon = '➖'; tag = '[   USED    ]'; }
+                    if (i.status === 'CANCELED') { icon = '❌'; tag = '[ CANCELED  ]'; }
                     
                     detailedOutput += `${icon} ${tag} ${i.number} | ${i.name}\n`;
                 });
